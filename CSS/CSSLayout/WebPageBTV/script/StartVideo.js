7778b00ac911b00383a6c5b81b92f54f0f387152
DimitarSD <dsd363978@yahoo.com>
2015-04-18 22:03:46 +0300

    Add CSSLayout homework

---
+++ CSS/CSSLayout/WebPageBTV/script/StartVideo.js
@@ -0,0 +1,25 @@
+﻿
+var myVideo = document.getElementById("BTV");
+
+var playButton = document.getElementById("PlayButton");
+playButton.onclick = play;
+
+myVideo.onclick = pause;
+
+var image = document.getElementById("NewsBTV");
+
+myVideo.onended = function () {
+    playButton.style.visibility = 'visible';
+    myVideo.style.visibility = 'hidden';
+}
+
+function play() {
+    myVideo.play();
+    myVideo.style.visibility = 'visible';
+    playButton.style.visibility = 'hidden'
+}
+
+function pause() {
+    myVideo.pause();
+    playButton.style.visibility = 'visible';
+}
