d9ac701e36cf7432b2d42c5dbd7a7cd94f8b6710
DimitarSD <dsd363978@yahoo.com>
2015-04-19 11:58:47 +0300

    Add CSS Preprocessors homework

---
+++ CSS/CSSPreprocessors/WebGallery/script/DisplayImage.js
@@ -0,0 +1,54 @@
+﻿var firstImage = document.getElementById("ImageOne");
+var firstImageDisplay = document.getElementById("ImageOneDisplay");
+
+var secondImage = document.getElementById("ImageTwo");
+var secondImageDisplay = document.getElementById("ImageTwoDisplay");
+
+var thirdImage = document.getElementById("ImageThree");
+var thirdImageDisplay = document.getElementById("ImageThreeDisplay");
+
+var fourthImage = document.getElementById("ImageFour");
+var fourthImageDisplay = document.getElementById("ImageFourDisplay");
+
+var fifthImage = document.getElementById("ImageFive");
+var fifthImageDisplay = document.getElementById("ImageFiveDisplay");
+
+firstImage.onclick = function () {
+    firstImageDisplay.style.visibility = 'visible';
+    secondImageDisplay.style.visibility = 'hidden';
+    thirdImageDisplay.style.visibility = 'hidden';
+    fourthImageDisplay.style.visibility = 'hidden';
+    fifthImageDisplay.style.visibility = 'hidden';
+}
+
+secondImage.onclick = function () {
+    firstImageDisplay.style.visibility = 'hidden';
+    secondImageDisplay.style.visibility = 'visible';
+    thirdImageDisplay.style.visibility = 'hidden';
+    fourthImageDisplay.style.visibility = 'hidden';
+    fifthImageDisplay.style.visibility = 'hidden';
+}
+
+thirdImage.onclick = function () {
+    firstImageDisplay.style.visibility = 'hidden';
+    secondImageDisplay.style.visibility = 'hidden';
+    thirdImageDisplay.style.visibility = 'visible';
+    fourthImageDisplay.style.visibility = 'hidden';
+    fifthImageDisplay.style.visibility = 'hidden';
+}
+
+fourthImage.onclick = function () {
+    firstImageDisplay.style.visibility = 'hidden';
+    secondImageDisplay.style.visibility = 'hidden';
+    thirdImageDisplay.style.visibility = 'hidden';
+    fourthImageDisplay.style.visibility = 'visible';
+    fifthImageDisplay.style.visibility = 'hidden';
+}
+
+fifthImage.onclick = function () {
+    firstImageDisplay.style.visibility = 'hidden';
+    secondImageDisplay.style.visibility = 'hidden';
+    thirdImageDisplay.style.visibility = 'hidden';
+    fourthImageDisplay.style.visibility = 'hidden';
+    fifthImageDisplay.style.visibility = 'visible';
+}
