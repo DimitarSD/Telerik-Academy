5119f385ce53ec65a0ec589b8c2ebde7f67c70f3
DimitarSD <dsd363978@yahoo.com>
2015-04-26 23:02:08 +0300

    Add more pictures and music

--- CSS/CSSPreprocessors/WebGallery/script/DisplayImage.js
+++ CSS/CSSPreprocessors/WebGallery/script/DisplayImage.js
@@ -13,12 +13,32 @@ var fourthImageDisplay = document.getElementById("ImageFourDisplay");
 var fifthImage = document.getElementById("ImageFive");
 var fifthImageDisplay = document.getElementById("ImageFiveDisplay");
 
+var sixthImage = document.getElementById("ImageSix");
+var sixthImageDisplay = document.getElementById("ImageSixDisplay");
+
+var seventhImage = document.getElementById("ImageSeven");
+var seventhImageDisplay = document.getElementById("ImageSevenDisplay");
+
+var eightImage = document.getElementById("ImageEight");
+var eightImageDisplay = document.getElementById("ImageEightDisplay");
+
+var ninethImage = document.getElementById("ImageNine");
+var ninethImageDisplay = document.getElementById("ImageNineDisplay");
+
+var tenImage = document.getElementById("ImageTen");
+var tenImageDisplay = document.getElementById("ImageTenDisplay");
+
 firstImage.onclick = function () {
     firstImageDisplay.style.visibility = 'visible';
     secondImageDisplay.style.visibility = 'hidden';
     thirdImageDisplay.style.visibility = 'hidden';
     fourthImageDisplay.style.visibility = 'hidden';
     fifthImageDisplay.style.visibility = 'hidden';
+    sixthImageDisplay.style.visibility = 'hidden';
+    seventhImageDisplay.style.visibility = 'hidden';
+    eightImageDisplay.style.visibility = 'hidden';
+    ninethImageDisplay.style.visibility = 'hidden';
+    tenImageDisplay.style.visibility = 'hidden';
 }
 
 secondImage.onclick = function () {
@@ -27,6 +47,11 @@ secondImage.onclick = function () {
     thirdImageDisplay.style.visibility = 'hidden';
     fourthImageDisplay.style.visibility = 'hidden';
     fifthImageDisplay.style.visibility = 'hidden';
+    sixthImageDisplay.style.visibility = 'hidden';
+    seventhImageDisplay.style.visibility = 'hidden';
+    eightImageDisplay.style.visibility = 'hidden';
+    ninethImageDisplay.style.visibility = 'hidden';
+    tenImageDisplay.style.visibility = 'hidden';
 }
 
 thirdImage.onclick = function () {
@@ -35,6 +60,11 @@ thirdImage.onclick = function () {
     thirdImageDisplay.style.visibility = 'visible';
     fourthImageDisplay.style.visibility = 'hidden';
     fifthImageDisplay.style.visibility = 'hidden';
+    sixthImageDisplay.style.visibility = 'hidden';
+    seventhImageDisplay.style.visibility = 'hidden';
+    eightImageDisplay.style.visibility = 'hidden';
+    ninethImageDisplay.style.visibility = 'hidden';
+    tenImageDisplay.style.visibility = 'hidden';
 }
 
 fourthImage.onclick = function () {
@@ -43,6 +73,11 @@ fourthImage.onclick = function () {
     thirdImageDisplay.style.visibility = 'hidden';
     fourthImageDisplay.style.visibility = 'visible';
     fifthImageDisplay.style.visibility = 'hidden';
+    sixthImageDisplay.style.visibility = 'hidden';
+    seventhImageDisplay.style.visibility = 'hidden';
+    eightImageDisplay.style.visibility = 'hidden';
+    ninethImageDisplay.style.visibility = 'hidden';
+    tenImageDisplay.style.visibility = 'hidden';
 }
 
 fifthImage.onclick = function () {
@@ -51,4 +86,74 @@ fifthImage.onclick = function () {
     thirdImageDisplay.style.visibility = 'hidden';
     fourthImageDisplay.style.visibility = 'hidden';
     fifthImageDisplay.style.visibility = 'visible';
+    sixthImageDisplay.style.visibility = 'hidden';
+    seventhImageDisplay.style.visibility = 'hidden';
+    eightImageDisplay.style.visibility = 'hidden';
+    ninethImageDisplay.style.visibility = 'hidden';
+    tenImageDisplay.style.visibility = 'hidden';
+}
+
+sixthImage.onclick = function () {
+    firstImageDisplay.style.visibility = 'hidden';
+    secondImageDisplay.style.visibility = 'hidden';
+    thirdImageDisplay.style.visibility = 'hidden';
+    fourthImageDisplay.style.visibility = 'hidden';
+    fifthImageDisplay.style.visibility = 'hidden';
+    sixthImageDisplay.style.visibility = 'visible';
+    seventhImageDisplay.style.visibility = 'hidden';
+    eightImageDisplay.style.visibility = 'hidden';
+    ninethImageDisplay.style.visibility = 'hidden';
+    tenImageDisplay.style.visibility = 'hidden';
+}
+
+seventhImage.onclick = function () {
+    firstImageDisplay.style.visibility = 'hidden';
+    secondImageDisplay.style.visibility = 'hidden';
+    thirdImageDisplay.style.visibility = 'hidden';
+    fourthImageDisplay.style.visibility = 'hidden';
+    fifthImageDisplay.style.visibility = 'hidden';
+    sixthImageDisplay.style.visibility = 'hidden';
+    seventhImageDisplay.style.visibility = 'visible';
+    eightImageDisplay.style.visibility = 'hidden';
+    ninethImageDisplay.style.visibility = 'hidden';
+    tenImageDisplay.style.visibility = 'hidden';
+}
+
+eightImage.onclick = function () {
+    firstImageDisplay.style.visibility = 'hidden';
+    secondImageDisplay.style.visibility = 'hidden';
+    thirdImageDisplay.style.visibility = 'hidden';
+    fourthImageDisplay.style.visibility = 'hidden';
+    fifthImageDisplay.style.visibility = 'hidden';
+    sixthImageDisplay.style.visibility = 'hidden';
+    seventhImageDisplay.style.visibility = 'hidden';
+    eightImageDisplay.style.visibility = 'visible';
+    ninethImageDisplay.style.visibility = 'hidden';
+    tenImageDisplay.style.visibility = 'hidden';
+}
+
+ninethImage.onclick = function () {
+    firstImageDisplay.style.visibility = 'hidden';
+    secondImageDisplay.style.visibility = 'hidden';
+    thirdImageDisplay.style.visibility = 'hidden';
+    fourthImageDisplay.style.visibility = 'hidden';
+    fifthImageDisplay.style.visibility = 'hidden';
+    sixthImageDisplay.style.visibility = 'hidden';
+    seventhImageDisplay.style.visibility = 'hidden';
+    eightImageDisplay.style.visibility = 'hidden';
+    ninethImageDisplay.style.visibility = 'visible';
+    tenImageDisplay.style.visibility = 'hidden';
+}
+
+tenImage.onclick = function () {
+    firstImageDisplay.style.visibility = 'hidden';
+    secondImageDisplay.style.visibility = 'hidden';
+    thirdImageDisplay.style.visibility = 'hidden';
+    fourthImageDisplay.style.visibility = 'hidden';
+    fifthImageDisplay.style.visibility = 'hidden';
+    sixthImageDisplay.style.visibility = 'hidden';
+    seventhImageDisplay.style.visibility = 'hidden';
+    eightImageDisplay.style.visibility = 'hidden';
+    ninethImageDisplay.style.visibility = 'hidden';
+    tenImageDisplay.style.visibility = 'visible';
 }
