4b0b94c5abd3e5463741783c31e8e091c5625ee1
DimitarSD <dsd363978@yahoo.com>
2015-05-30 12:38:19 +0300

    Delete some old folders

--- JavaScript UI & DOM/DocumentObjectModel/DocumentObjectModel/02.GetValue/script/exercise-solution.js
+++
@@ -1,9 +0,0 @@
-﻿// 02. Create a function that gets the value of <input type="text"> ands prints its value to the console
-var jsConsole;
-var extractButton = document.getElementById('extract-value');
-
-extractButton.onclick = function () {
-    var input = document.getElementById('input-text');
-    var text = input.value;
-    jsConsole.writeLine(text);
-}
--- JavaScript UI & DOM/DocumentObjectModel/DocumentObjectModel/03.GetColor/script/exercise-solution.js
+++
@@ -1,8 +0,0 @@
-﻿// 03. Crеate a function that gets the value of <input type="color"> and sets the background of the body to this value
-var setColorButton = document.getElementById('set-color');
-
-setColorButton.onclick = function () {
-    var color = document.getElementById('input-text').value;
-
-    document.body.style.backgroundColor = '#' + color;
-};
