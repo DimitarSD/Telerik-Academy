cada64971631af3c6fb8f6369652b774fa83807c
DimitarSD <dsd363978@yahoo.com>
2015-07-02 11:04:53 +0300

    Variables, Data, Expressions and Constants homework

---
+++ 01. Programming/04. High-Quality Code [C#, JavaScript]/04. Variables, Data, Expressions and Constants/ClassSize/Size.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassSize
+{
+    using System;
+
+    public class Size
+    {
+        private double width;
+        private double height;
+
+        public Size(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public static Size GetRotatedSize(Size figure, double angleOfRotation)
+        {
+            double angleOfRotationCos = Math.Cos(angleOfRotation);
+            double angleOfRotationSin = Math.Cos(angleOfRotation);
+
+            double newWidth = Math.Abs(angleOfRotationCos) * figure.width + Math.Abs(angleOfRotationSin) * figure.height;
+            double newHeight = Math.Abs(angleOfRotationSin) * figure.width + Math.Abs(angleOfRotationCos) * figure.height;
+
+            return new Size(newWidth, newHeight);
+        }
+    }
+}
---
+++ 01. Programming/04. High-Quality Code [C#, JavaScript]/04. Variables, Data, Expressions and Constants/PrintStatistics/Statistic.cs
@@ -0,0 +1,53 @@
+﻿namespace PrintStatistics
+{
+    using System;
+    using System.Text;
+
+    public class Statistic
+    {
+        public string PrintStatistics(double[] dataColection, int numberOfAllRecords)
+        {
+            StringBuilder resultFromStatistic = new StringBuilder();
+
+            double maxValue = Double.MinValue;
+
+            for (int i = 0; i < numberOfAllRecords; i++)
+            {
+                if (dataColection[i] > maxValue)
+                {
+                    maxValue = dataColection[i];
+                }
+            }
+            
+            string maxValueAsString = string.Format("The maximum value from the given data is {0}", maxValue);
+            resultFromStatistic.AppendLine(maxValueAsString);
+
+            double minValue = Double.MaxValue;
+
+            for (int i = 0; i < numberOfAllRecords; i++)
+            {
+                if (dataColection[i] < minValue)
+                {
+                    minValue = dataColection[i];
+                }
+            }
+
+            string minValueAsString = string.Format("The minimum value from the given data is {0}", minValue);
+            resultFromStatistic.AppendLine(minValueAsString);
+
+            double sum = 0;
+
+            for (int i = 0; i < numberOfAllRecords; i++)
+            {
+                sum += dataColection[i];
+            }
+
+            double averageValue = sum / numberOfAllRecords;
+
+            string averageValueAsString = string.Format("The average value from the given data is {0}", averageValue);
+            resultFromStatistic.AppendLine(averageValueAsString);
+
+            return resultFromStatistic.ToString();
+        }
+    }
+}
