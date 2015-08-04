193c81113726b1851faca74b309c03505776f67c
DimitarSD <dsd363978@yahoo.com>
2015-08-04 17:47:27 +0300

    Added HTML Templates homework

---
+++ 02. Web Development/05. JavaScript UI & DOM/11. HTML Templates/tasks/task-3.js
@@ -0,0 +1,17 @@
+/* globals $ */
+
+function solve() {
+  return function () {
+    $.fn.listview = function (data) {
+      var $this = $(this);
+      var id = '#' + $this.attr('data-template');
+      var template = $(id).html();
+      var compiledTemplate = handlebars.compile(template);
+      data.forEach(function (item){
+        $this.append(compiledTemplate(item));
+      });
+    };
+  };
+}
+
+module.exports = solve;
