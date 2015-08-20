4fecb707369c975f171fa0c945f769f466d2fc35
DimitarSD <dsd363978@yahoo.com>
2015-08-20 12:46:07 +0300

    Added the last 5 homeworks

---
+++ 01. Programming/04. High-Quality Code [C#, JavaScript]/12. Refactoring/RotatingWalkInMatrix/RotatingWalkInMatrix.Tests/RotatingWalkInMatrixTests.cs
@@ -0,0 +1,108 @@
+﻿namespace RotatingWalkInMatrix.Tests
+{
+    using System;
+
+    using RotatingWalkInMatrix;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RotatingWalkInMatrixTests
+    {
+        [TestMethod]
+        [Timeout(100)]
+        public void ZeroSizedMatrixTest()
+        {
+            byte n = 0;
+            var expectedMatrix = new int[n, n];
+            var actualMatrix = MatrixTraversal.GenerateMatrix(n);
+
+            var areMatrixesEqual = this.AreMatricesEqual(expectedMatrix, actualMatrix);
+            Assert.IsTrue(areMatrixesEqual);
+        }
+
+        [TestMethod]
+        [Timeout(100)]
+        public void OneSizedMatrixTest()
+        {
+            byte n = 1;
+            var expectedMatrix = new int[,]
+            {
+                { 1 }
+            };
+            var actualMatrix = MatrixTraversal.GenerateMatrix(n);
+
+            var areMatrixesEqual = this.AreMatricesEqual(expectedMatrix, actualMatrix);
+            Assert.IsTrue(areMatrixesEqual);
+        }
+
+        [TestMethod]
+        [Timeout(100)]
+        public void TwoSizedMatrixTest()
+        {
+            byte n = 2;
+            var expectedMatrix = new int[,] 
+            {
+                { 1, 4 },
+                { 3, 2 }
+            };
+            var actualMatrix = MatrixTraversal.GenerateMatrix(n);
+
+            var areMatrixesEqual = this.AreMatricesEqual(expectedMatrix, actualMatrix);
+            Assert.IsTrue(areMatrixesEqual);
+        }
+
+        [TestMethod]
+        [Timeout(100)]
+        public void ThreeSizedMatrixTest()
+        {
+            byte n = 3;
+            var expectedMatrix = new int[,] 
+            {
+                { 1, 7, 8 },
+                { 6, 2, 9 },
+                { 5, 4, 3 }
+            };
+            var actualMatrix = MatrixTraversal.GenerateMatrix(n);
+
+            var areMatrixesEqual = this.AreMatricesEqual(expectedMatrix, actualMatrix);
+            Assert.IsTrue(areMatrixesEqual);
+        }
+
+        [TestMethod]
+        [Timeout(100)]
+        public void SixSizedMatrixTest()
+        {
+            byte n = 6;
+            var expectedMatrix = new int[,] 
+            {
+                { 1, 16, 17, 18, 19, 20 },
+                { 15, 2, 27, 28, 29, 21 },
+                { 14, 31, 3, 26, 30, 22 },
+                { 13, 36, 32, 4, 25, 23 },
+                { 12, 35, 34, 33, 5, 24 },
+                { 11, 10, 9, 8, 7, 6 }
+            };
+            var actualMatrix = MatrixTraversal.GenerateMatrix(n);
+
+            var areMatrixesEqual = this.AreMatricesEqual(expectedMatrix, actualMatrix);
+            Assert.IsTrue(areMatrixesEqual);
+        }
+
+        private bool AreMatricesEqual(int[,] expectedMatrix, int[,] actualMatrix)
+        {
+            for (int i = 0; i < expectedMatrix.GetLongLength(0); i++)
+            {
+                for (int j = 0; j < expectedMatrix.GetLongLength(1); j++)
+                {
+                    if (expectedMatrix[i, j] != actualMatrix[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 01. Programming/04. High-Quality Code [C#, JavaScript]/12. Refactoring/RotatingWalkInMatrix/RotatingWalkInMatrix/MatrixTraversal.cs
@@ -0,0 +1,101 @@
+﻿namespace RotatingWalkInMatrix
+{
+    public class MatrixTraversal
+    {
+        private static readonly int[] DirectionsX = { 1, 0, -1, -1, 0, 1, -1, 0 };
+        private static readonly int[] DirectionsY = { 1, -1, 0, 1, 1, 0, -1, 1 };
+
+        public static int[,] GenerateMatrix(byte size)
+        {
+            int[,] matrix = new int[size, size];
+
+            Cell startupCell = FindFirstEmptyCellIfExists(matrix);
+            if (startupCell != null)
+            {
+                FillMatrix(matrix, startupCell);
+            }
+
+            return matrix;
+        }
+
+        private static void FillMatrix(int[,] matrix, Cell startupCell)
+        {
+            var currentCell = startupCell;
+            var dirIndex = 0;
+
+            while (IsCellPassable(matrix, currentCell))
+            {
+                matrix[currentCell.X, currentCell.Y] = currentCell.Value;
+
+                while (!IsNextCellPassable(matrix, currentCell, dirIndex) &&
+                       CanCellMoveSomewhere(matrix, currentCell, dirIndex))
+                {
+                    dirIndex = (dirIndex + 1) % DirectionsX.Length;
+                }
+
+                currentCell.X += DirectionsX[dirIndex];
+                currentCell.Y += DirectionsY[dirIndex];
+                currentCell.Value++;
+            }
+
+            var nextStartupCell = FindFirstEmptyCellIfExists(matrix);
+            if (nextStartupCell != null)
+            {
+                nextStartupCell.Value = currentCell.Value;
+                FillMatrix(matrix, nextStartupCell);
+            }
+        }
+
+        private static Cell FindFirstEmptyCellIfExists(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        var firstEmptyCell = new Cell(i, j);
+                        return firstEmptyCell;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static bool IsCellPassable(int[,] matrix, Cell currentCell)
+        {
+            bool isCellPassable = currentCell.X >= 0 && currentCell.X < matrix.GetLongLength(0) &&
+                                  currentCell.Y >= 0 && currentCell.Y < matrix.GetLongLength(1) &&
+                                  matrix[currentCell.X, currentCell.Y] == 0;
+
+            return isCellPassable;
+        }
+
+        private static bool IsNextCellPassable(int[,] matrix, Cell currentCell, int dirIndex)
+        {
+            Cell nextCell = new Cell()
+            {
+                X = currentCell.X + DirectionsX[dirIndex],
+                Y = currentCell.Y + DirectionsY[dirIndex]
+            };
+
+            return IsCellPassable(matrix, nextCell);
+        }
+
+        private static bool CanCellMoveSomewhere(int[,] matrix, Cell currentCell, int dirIndex)
+        {
+            for (int i = 0; i < DirectionsX.Length; i++)
+            {
+                dirIndex = (dirIndex + 1) % DirectionsX.Length;
+
+                if (IsNextCellPassable(matrix, currentCell, dirIndex))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
