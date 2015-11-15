a4c4503e73152ae0edfdfde99577984b19616177
dimitarsd <dsd363978@yahoo.com>
2015-11-15 17:30:44 +0200

    Added Graphs And Graph Algorithms homework

---
+++ 01. Programming/05. Data Structures and Algorithms [C#]/10. Graphs And Graph Algorithms/Salaries/StartUp.cs
@@ -0,0 +1,82 @@
+﻿namespace Salaries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /// <summary>
+    /// Using adjancency list and recursively DFS
+    /// </summary>
+    /// <see cref="http://bgcoder.com/Contests/63/Telerik-Algo-Academy-February-2013"/>
+    public class StartUp
+    {
+        private static readonly IDictionary<int, List<int>> AdjacencyList = new Dictionary<int, List<int>>();
+        private static long[] employees;
+
+        public static void Main()
+        {
+            #if DEBUG
+            Console.SetIn(new StreamReader("../../input.txt"));
+            #endif
+
+            ParseInput();
+            Console.WriteLine("Total salary: " + CalculateTotalSalary());
+        }
+
+        /// <summary>
+        /// Parse the input line by line. 
+        /// Use dictionary to save number of employees
+        /// </summary>
+        private static void ParseInput()
+        {
+            var numberOfEmployees = int.Parse(Console.ReadLine());
+            employees = new long[numberOfEmployees];
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                var inputLine = Console.ReadLine();
+                AdjacencyList[i] = new List<int>();
+
+                for (int j = 0; j < numberOfEmployees; j++)
+                {
+                    if (inputLine[j] == 'Y')
+                    {
+                        AdjacencyList[i].Add(j);
+                    }
+                }
+
+                if (AdjacencyList[i].Count == 0)
+                {
+                    employees[i] = 1;
+                }
+            }
+        }
+
+        private static long CalculateTotalSalary()
+        {
+            long totalSalary = 0;
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                totalSalary += Calculate(i);
+            }
+
+            return totalSalary;
+        }
+        
+        private static long Calculate(int employeeId)
+        {
+            if (employees[employeeId] != 0)
+            {
+                return employees[employeeId];
+            }
+
+            foreach (var employee in AdjacencyList[employeeId])
+            {
+                employees[employeeId] += Calculate(employee);
+            }
+
+            return employees[employeeId];
+        }
+    }
+}
---
+++ 01. Programming/05. Data Structures and Algorithms [C#]/10. Graphs And Graph Algorithms/TvCompany/StartUp.cs
@@ -0,0 +1,102 @@
+﻿namespace TvCompany
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// Using Kruskal to find Minimal Spanning Tree
+    /// </summary>
+    /// <see cref="https://en.wikipedia.org/wiki/Kruskal%27s_algorithm"/>
+    /// <seealso cref="https://en.wikipedia.org/wiki/Minimum_spanning_tree"/>
+    public class StartUp
+    {
+        private static Tuple<int, int, int>[] paths;
+        private static ISet<int> houses;
+
+        public static void Main()
+        {
+            #if DEBUG
+            Console.SetIn(new StreamReader("../../input.txt"));
+            #endif
+
+            ParseInput();
+
+            var joinByLine = Environment.NewLine;
+            var pathCollection = paths.Select(a => string.Format("[{0} {1} -> {2}]", a.Item1, a.Item2, a.Item3));
+            Console.WriteLine(string.Join(Environment.NewLine, pathCollection));
+
+            var allTrees = RepresendEachNodeAsTree();
+
+            double result = FindMinimalCost(allTrees);
+
+            Console.WriteLine("\nMinimal cost for cable: " + result);
+        }
+
+        private static HashSet<ISet<int>> RepresendEachNodeAsTree()
+        {
+            var allTrees = new HashSet<ISet<int>>();
+
+            // Represend each node as tree
+            foreach (var node in houses)
+            {
+                var tree = new HashSet<int>();
+                tree.Add(node);
+
+                allTrees.Add(tree);
+            }
+
+            return allTrees;
+        }
+
+        private static double FindMinimalCost(HashSet<ISet<int>> allTrees)
+        {
+            // Kruskal -> Sorting edges by their weight
+            Array.Sort(paths, (a, b) => a.Item3.CompareTo(b.Item3));
+
+            double result = 0;
+
+            foreach (var path in paths)
+            {
+                var treeOne = allTrees.Where(tree => tree.Contains(path.Item1)).First();
+                var treeTwo = allTrees.Where(tree => tree.Contains(path.Item2)).First();
+
+                // Elements are in same tree
+                if (treeOne.Equals(treeTwo))
+                {
+                    continue;
+                }
+
+                result += path.Item3;
+
+                treeOne.UnionWith(treeTwo);
+                allTrees.Remove(treeTwo);
+
+                // Small optimization
+                if (allTrees.Count == 1)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private static void ParseInput()
+        {
+            int numberOfHouses = int.Parse(Console.ReadLine());
+            paths = new Tuple<int, int, int>[numberOfHouses];
+            houses = new HashSet<int>();
+
+            for (int i = 0; i < numberOfHouses; i++)
+            {
+                var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                paths[i] = new Tuple<int, int, int>(input[0], input[1], input[2]);
+                houses.Add(input[0]);
+                houses.Add(input[1]);
+            }
+        }
+    }
+}
